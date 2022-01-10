1fb4035636a4608f22ef9e98eff8cdbf4a2937bc
Cstem <chrisstemitsiotis@gmail.com>
2022-01-10 21:04:19 +0200

    lshupdate!

--- importArticles.py
+++ importArticles.py
@@ -5,6 +5,7 @@ df = pd.read_csv('articles_data.csv')
 articlesDataframe = df[['source_name','author','title','description']]
 
 articlesDataframe.dropna(inplace=True)
+articlesDataframe = articlesDataframe.head(10)
 print(articlesDataframe)
 
 
